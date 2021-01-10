acffe6b13f5aeb7a6c5ee7d874438ada8ce3f3b4
Guilhelm Savin <contact@gsav.in>
2021-01-10 12:41:42 +0100

    Use localStorage to cache data

--- assets/scripts/nomadogeeks-common.js
+++ assets/scripts/nomadogeeks-common.js
@@ -1,5 +1,60 @@
 "use strict";
 
+// See https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    var x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch(e) {
+    return e instanceof DOMException && (
+      // everything except Firefox
+      e.code === 22 ||
+      // Firefox
+      e.code === 1014 ||
+      // test name field too, because code might not be present
+      // everything except Firefox
+      e.name === 'QuotaExceededError' ||
+      // Firefox
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      (storage && storage.length !== 0);
+    }
+}
+
+const hasStorage = storageAvailable("localStorage");
+const build = document.querySelector("body").getAttribute("data-build");
+
+export function loadFromStorage(key) {
+  if (hasStorage) {
+    try {
+      const existingDataTimestamp = localStorage.getItem(key + ":timestamp");
+      const existingData = localStorage.getItem(key);
+
+      if (existingDataTimestamp == build && existingData) {
+        console.debug(`${key} loaded from storage`);
+        return JSON.parse(existingData);
+      }
+    } catch (e) {
+      console.err("Unable to load data from storage", e);
+    }
+  }
+}
+
+export function storeData(key, data) {
+  if (hasStorage) {
+    try {
+      localStorage.setItem(key + ":timestamp", build);
+      localStorage.setItem(key, JSON.stringify(data));
+    } catch (e) {
+      console.err(`Unable to store ${key} in storage`, e);
+    }
+  }
+}
+
 export function debounce(callback, delay) {
   let timeout;
   return function () {
@@ -14,6 +69,14 @@ export function debounce(callback, delay) {
 
 export function fetchData (store, key, transform) {
   store.loading = true;
+  const existingData = loadFromStorage(key);
+
+  if (existingData) {
+    store.data = existingData;
+    store.error = null;
+    store.loading = false;
+    return Promise.resolve();
+  }
 
   return fetch(`/assets/data/${key}.json`).then(response => {
     store.loading = false;
@@ -25,6 +88,7 @@ export function fetchData (store, key, transform) {
       return response.json().then(data => {
         store.error = null;
         store.data = transform ? transform(data) : data[key];
+        storeData(key, store.data);
       });
     }
   })
