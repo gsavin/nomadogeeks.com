ca5177169fded018c4b7d0f17b2d6c5c680bb248
Guilhelm Savin <contact@gsav.in>
2021-04-18 22:10:10 +0200

    Add image transformation tool

---
+++ tools/resize.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+WATERMARK=$(dirname $0)/watermark.png
+OPTS="-quality 85"
+
+for img in $* ; do
+	width=`convert $img -ping -format "%w" info:`
+	height=`convert $img -ping -format "%h" info:`
+	echo "${img}:${width}x${height}"
+
+	if [ $width -lt $height ]; then
+		resize='1024x'
+	else
+		resize='x1024'
+	fi
+
+	final=`basename $img`
+	final="`date +%F`-${final:0:(-4)}.jpg"
+	convert -resize "$resize" $OPTS $img tmp-$final
+#	composite -dissolve 35% -gravity SouthEast watermark.png tmp-$final $final
+	composite -gravity SouthEast ${WATERMARK} tmp-$final $final
+	rm tmp-$final 
+done
+
