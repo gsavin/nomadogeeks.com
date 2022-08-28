8e439c6de353b61906f35b2de13f5190491dd32f
Guilhelm Savin <contact@gsav.in>
2022-08-28 19:59:10 +0200

    Add tool to init a collection

---
+++ tools/init-collections.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+country=$1
+post=$2
+
+collection=$(echo $post | tr / -)
+
+for f in $(find assets/images/${post} -type f | sort); do
+	id=${f##assets/images};
+	id=${id%%.jpg};
+	if [ $(basename ${id}) = "preview" -o $(basename ${id}) = "preview-full" ]; then continue; fi
+	imgdate=$(exiv2 -K Exif.Image.DateTimeOriginal -P v ${f} | awk '{gsub(/:/, "/", $1)} 1')
+	latitude=$(exiv2 -P v -K Xmp.drone-dji.Latitude ${f})
+	longitude=$(exiv2 -P v -K Xmp.drone-dji.Longitude ${f})
+
+	cat <<EOF
+- id: ${id}
+  path: /$f
+  name:
+  date: ${imgdate}
+  description:
+  location:
+  country: $country
+  tags:
+    - $country
+EOF
+	if [ -n "$latitude" ]; then
+		echo "  latitude: ${latitude##+}"
+		echo "  longitude: ${longitude##+}"
+	fi
+done > _data/images/${collection}.yml
