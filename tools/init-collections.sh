87bf1a8ffdac1d7480b3d9bfc32c593770bcad86
Guilhelm Savin <contact@gsav.in>
2022-09-03 13:45:25 +0200

    Improve GPS information

--- tools/init-collections.sh
+++ tools/init-collections.sh
@@ -5,13 +5,38 @@ post=$2
 
 collection=$(echo $post | tr / -)
 
+gps_info() {
+	t=$1
+	f=$2
+	l=$(exiv2 -P v -K Xmp.drone-dji.${t} ${f})
+
+	if [ -z "$l" ]; then
+		l=$(exiv2 -P v -K Exif.GPSInfo.GPS${t} ${f} | sed 's%^\([0-9]\+\)/\([0-9]\+\) \([0-9]\+\)/\([0-9]\+\) \([0-9]\+\)/\([0-9]\+\)$%\1 \2 \3 \4 \5 \6%' | awk '{ print $1/$2+$3/$4/60.0+$5/$6/3600.0 }')
+		if [ -n "$l" ]; then
+			d=$(exiv2 -P v -K Exif.GPSInfo.GPS${t}Ref ${f} | tr '[:upper:]' '[:lower:]')
+			if [ "$d" = "s" -o "$d" = "w" ]; then
+				l="-${l}"
+			fi
+		fi
+	fi
+	echo $l
+}
+
+latitude() {
+	gps_info Latitude $1
+}
+
+longitude() {
+	gps_info Longitude $1
+}
+
 for f in $(find assets/images/${post} -type f | sort); do
 	id=${f##assets/images};
 	id=${id%%.jpg};
 	if [ $(basename ${id}) = "preview" -o $(basename ${id}) = "preview-full" ]; then continue; fi
 	imgdate=$(exiv2 -K Exif.Photo.DateTimeOriginal -K Exif.Image.DateTimeOriginal -P v ${f} | head -n1 | awk '{gsub(/:/, "/", $1)} 1')
-	latitude=$(exiv2 -P v -K Xmp.drone-dji.Latitude ${f})
-	longitude=$(exiv2 -P v -K Xmp.drone-dji.Longitude ${f})
+	latitude=$(latitude ${f})
+	longitude=$(longitude ${f})
 
 	cat <<EOF
 - id: ${id}
